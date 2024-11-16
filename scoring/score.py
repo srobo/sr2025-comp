a5fc2b7c81eb7d194565cdb02fca4d553d977aee
Peter Law <PeterJCLaw@gmail.com>
2024-11-16 18:24:44 +0000

    Add initial input validation

--- scoring/score.py
+++ scoring/score.py
@@ -7,9 +7,11 @@ Required as part of a compstate.
 from __future__ import annotations
 
 import collections
-import warnings
+from typing import Iterable
 
 from sr2025 import DISTRICT_SCORE_MAP, RawDistrict, ZONE_COLOURS
+
+TOKENS_PER_ZONE = 6
 
 
 class District(RawDistrict):
@@ -22,6 +24,29 @@ class InvalidScoresheetException(Exception):
         self.code = code
 
 
+def join_text(strings: Iterable[str], separator: str) -> str:
+    strings = tuple(strings)
+
+    try:
+        *strings, right = strings
+    except TypeError:
+        return ""
+
+    if not strings:
+        return right
+
+    left = ", ".join(strings[:-1])
+    return f"{left} {separator} {strings[-1]}"
+
+
+def join_and(strings: Iterable[str]) -> str:
+    return join_text(strings, "and")
+
+
+def join_or(strings: Iterable[str]) -> str:
+    return join_text(strings, "or")
+
+
 class Scorer:
     def __init__(self, teams_data, arena_data):
         self._teams_data = teams_data
@@ -31,6 +56,7 @@ class Scorer:
             district['pallet_counts'] = collections.Counter(
                 district['pallets'].replace(' ', ''),
             )
+            district['highest'] = district['highest'].replace(' ', '')
 
     def score_district_for_zone(self, name: str, district: District, zone: int) -> int:
         colour = ZONE_COLOURS[zone]
@@ -56,7 +82,95 @@ class Scorer:
         return scores
 
     def validate(self, other_data):
-        warnings.warn("Scoresheet validation not implemented")
+        if self._districts.keys() != DISTRICT_SCORE_MAP.keys():
+            missing = DISTRICT_SCORE_MAP.keys() - self._districts.keys()
+            extra = self._districts.keys() - DISTRICT_SCORE_MAP.keys()
+            raise InvalidScoresheetException(
+                f"Wrong districts specified. Missing: {join_and(missing)}. "
+                f"Extra: {extra!r}",
+                code='invalid_districts',
+            )
+
+        bad_highest = {}
+        for name, district in self._districts.items():
+            highest = district['highest']
+            if highest and highest not in ZONE_COLOURS:
+                bad_highest[name] = highest
+        if bad_highest:
+            raise InvalidScoresheetException(
+                f"Invalid pallets specified as the highest in some districts -- "
+                f"must be a single pallet from "
+                f"{join_or(repr(x) for x in ZONE_COLOURS)}.\n"
+                f"{bad_highest!r}",
+                code='invalid_highest_pallet',
+            )
+
+        bad_pallets = {}
+        for name, district in self._districts.items():
+            extra = district['pallet_counts'].keys() - ZONE_COLOURS
+            if extra:
+                bad_pallets[name] = extra
+        if bad_pallets:
+            raise InvalidScoresheetException(
+                f"Invalid pallets specified in some districts -- must be from "
+                f"{join_or(repr(x) for x in ZONE_COLOURS)}.\n"
+                f"{bad_pallets!r}",
+                code='invalid_pallets',
+            )
+
+        bad_highest2 = {}
+        for name, district in self._districts.items():
+            highest = district['highest']
+            if highest and highest not in district['pallet_counts']:
+                bad_highest2[name] = (highest, district['pallet_counts'].keys())
+        if bad_highest2:
+            detail = "\n".join(
+                (
+                    (
+                        f"District {name} has only {join_and(pallets)} so "
+                        f"{highest!r} cannot be the highest."
+                    )
+                    if pallets
+                    else (
+                        f"District {name} has no pallets so {highest!r} cannot "
+                        "be the highest."
+                    )
+                )
+                for name, (highest, pallets) in bad_highest2.items()
+            )
+            raise InvalidScoresheetException(
+                f"Impossible pallets specified as the highest in some districts "
+                f"-- must be a pallet which is present in the district.\n"
+                f"{detail}",
+                code='impossible_highest_pallet',
+            )
+
+        missing_highest = {}
+        for name, district in self._districts.items():
+            highest = district['highest']
+            pallet_counts = district['pallet_counts']
+            if len(pallet_counts) == 1 and not highest:
+                pallet, = pallet_counts.keys()
+                missing_highest[name] = pallet
+        if missing_highest:
+            raise InvalidScoresheetException(
+                f"Some districts with pallets from a single team are missing "
+                "specification of the highest.\n"
+                f"{missing_highest!r}",
+                code='missing_highest_pallet',
+            )
+
+        totals = collections.Counter()
+        for district in self._districts.values():
+            totals += district['pallet_counts']
+        too_many = {x for x, y in totals.items() if y > TOKENS_PER_ZONE}
+        if too_many:
+            raise InvalidScoresheetException(
+                f"Too many pallets of some kinds specified, must be no more "
+                f"than {TOKENS_PER_ZONE} of each type.\n"
+                f"{too_many!r}",
+                code='too_many_pallets',
+            )
 
 
 if __name__ == '__main__':
